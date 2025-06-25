78b6f12c044373e766db5ca5ab408d78e0fbcc28
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:11:34 +0530

    Create Question_33.py

---
+++ Assignment/Module 7 Introduction to Python/Question_33.py
@@ -0,0 +1,20 @@
+# Q33. Write a Python program to check a list is empty or not. 
+#Answer: check if a list is empty or not
+
+# 1.Name list
+
+name=["Aditya"]
+
+if not name:
+    print("The name list is empty.")
+else:
+    print("The name list is not empty.")
+
+#2. City list
+
+city=[]
+
+if not city:
+    print("The city list empty.")
+else:
+    print("The city list is not empty.")
