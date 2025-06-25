c552832df5f16aa67ab9cf5a08abb9f2d3a52361
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:09:35 +0530

    Create Question_8.py

---
+++ Assignment/Module 7 Introduction to Python/Question_8.py
@@ -0,0 +1,11 @@
+# Q8. What is the purpose continuing statement in python?
+#Answer:
+#The continue statement skips the current loop iteration and moves to the next one.
+
+
+for i in range(5):
+    if i == 1:
+        continue
+    print(i)
+
+
