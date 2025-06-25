40432aaea67bb9bdce1cf8a5d44b573dd42b4021
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:09:32 +0530

    Create Question_7.py

---
+++ Assignment/Module 7 Introduction to Python/Question_7.py
@@ -0,0 +1,5 @@
+# Q7: How is memory managed in Python?
+# Answer:
+# Python manages memory automatically using a private heap.
+# It uses reference counting and garbage collection to free unused memory.
+
