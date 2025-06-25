ef4b14c4b9dc74ef8789b21761568f83ace38673
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:08:34 +0530

    Create Question_1.py

---
+++ Assignment/Module 7 Introduction to Python/Question_1.py
@@ -0,0 +1,8 @@
+# Q1. What are the types of Applications?
+# Answer: 1) Web Applications
+# 2) Desktop Applications
+# 3) Mobile Applications
+# 4) Console Applications
+# 5) Game Applications
+# 6) Embedded Systems Applications
+
