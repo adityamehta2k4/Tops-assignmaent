e340d7d8ea4ae3eb65fed0de05ced23040b1436c
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:11:05 +0530

    Create Question_25.py

---
+++ Assignment/Module 7 Introduction to Python/Question_25.py
@@ -0,0 +1,12 @@
+# Q25.What is List? How will you reverse a list?
+#Answer: A list is a collection in Python that can store multiple items in a single variable.
+# Lists can store items of any data type (numbers, strings, etc.)
+# Lists are ordered, changeable, and allow duplicate values.
+# Lists are written using square brackets [].
+
+#reverse
+
+fruits=["apple","mango","banana"]
+fruits.reverse()
+print(fruits)
+
