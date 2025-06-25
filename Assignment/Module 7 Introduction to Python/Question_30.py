1f4e8f433e1b01a283e29f98853728b4166a5c35
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:11:23 +0530

    Create Question_30.py

---
+++ Assignment/Module 7 Introduction to Python/Question_30.py
@@ -0,0 +1,26 @@
+# Q30. How will you compare two lists?
+# Answer:
+
+#same
+list1=[50,6,78]
+list2=[50,6,78]
+if list1 == list2:
+    print("Lists are exactly the same")
+else:
+    print("Lists are different")
+
+#different
+list3=[85,60,75]
+list4=[65,40,12]
+if list3 == list4:
+    print("Lists are exactly the same")
+else:
+    print("Lists are different")
+    
+    
+    
+
+
+
+
+
