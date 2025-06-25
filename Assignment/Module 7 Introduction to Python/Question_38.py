f5cb8c30bbc409bda9361ddb4e01547f333ab234
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:12:02 +0530

    Create Question_38.py

---
+++ Assignment/Module 7 Introduction to Python/Question_38.py
@@ -0,0 +1,10 @@
+# Q38. Write a Python program to select an item randomly from a list.
+#Answer:
+
+import random
+
+my_city=['Ahmedabad','Surat','Rajkot','Baroda']
+
+random_city=random.choice(my_city)
+
+print("Randomlu Selected item:",random_city)
