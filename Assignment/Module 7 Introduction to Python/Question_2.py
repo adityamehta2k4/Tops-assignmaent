45a8b31ea94451e759ef98ce8a6f9a46f8e673b3
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:08:54 +0530

    Create Question_2.py

---
+++ Assignment/Module 7 Introduction to Python/Question_2.py
@@ -0,0 +1,3 @@
+# Q2. What is Programming?
+# Answer: Programming is the process of writing instructions (code) that a computer can understand and execute.
+# It helps us automate tasks, build software, websites, and more.
