f6e98fa243a4703d490f9be9670fecbec8b04f68
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:11:08 +0530

    Create Question_26.py

---
+++ Assignment/Module 7 Introduction to Python/Question_26.py
@@ -0,0 +1,18 @@
+# Q26.  How will you remove last object from a list?
+# Answer:
+
+
+# remove the last item
+
+# Method 1:
+
+
+list=[10,23,25,30,21,25]
+list.pop()
+print(list)
+
+# Method 2:
+
+list=[58,69,45,85,35,15]
+list=list[:-1]
+print(list)
