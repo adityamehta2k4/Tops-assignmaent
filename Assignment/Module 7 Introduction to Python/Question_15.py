ceb84ac1f088e969dd9a5cf2bcefa7580ba7212a
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:10:06 +0530

    Create Question_15.py

---
+++ Assignment/Module 7 Introduction to Python/Question_15.py
@@ -0,0 +1,10 @@
+#Q 15: Write a Python program to calculate the length of a string.
+#Answer:
+
+text=input("Enter a string:")
+
+lenght=len(text)
+
+print("The lenght of the string is:",lenght)
+
+
