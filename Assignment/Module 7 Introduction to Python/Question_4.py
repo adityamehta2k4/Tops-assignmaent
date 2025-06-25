55e697d471f98056b9a3d9ddf5b02ff53ffcecee
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:09:05 +0530

    Create Question_4.py

---
+++ Assignment/Module 7 Introduction to Python/Question_4.py
@@ -0,0 +1,11 @@
+# Q4.Write a Python program to check if a number is positive, negative, or zero.
+#Answer:
+num=float(input("Enter a number : "))
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+
