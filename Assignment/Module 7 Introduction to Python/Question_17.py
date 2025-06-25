51b6745f3e203196e6b08158bfb8d0a2808b6860
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:10:16 +0530

    Create Question_17.py

---
+++ Assignment/Module 7 Introduction to Python/Question_17.py
@@ -0,0 +1,13 @@
+# Q17. What are negative indexes and why are they used?
+# Answer:
+
+# Negative indexes in Python are used to access elements from the end of a list, string, or tuple.
+# -1 refers to the last element, -2 to the second last, and so on.
+
+text="Aditya"
+print(text[-1])
+print(text[-2])
+print(text[-3])
+print(text[-4])
+print(text[-5])
+print(text[-6])
