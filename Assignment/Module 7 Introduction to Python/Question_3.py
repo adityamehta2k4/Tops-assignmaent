8f111c250934c7a60a0fc66663b4166f0de32ad2
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:08:59 +0530

    Create Question_3.py

---
+++ Assignment/Module 7 Introduction to Python/Question_3.py
@@ -0,0 +1,3 @@
+# Q3. What is Python?
+# Answer: Python is a high-level, interpreted programming language known for its readability and simplicity.
+# It is widely used in web development, data science, machine learning, and scripting.
