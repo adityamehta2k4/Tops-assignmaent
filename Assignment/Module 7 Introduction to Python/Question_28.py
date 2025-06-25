bac801a670a5b1d1cac007d221ea1d5ee200a555
Aditya Mehta <adityamehta2k4@gmail.com>
2025-06-26 01:11:15 +0530

    Create Question_28.py

---
+++ Assignment/Module 7 Introduction to Python/Question_28.py
@@ -0,0 +1,19 @@
+# Q28.Differentiate between append () and extend () methods? 
+#Answer:
+
+#append()
+
+list1=[85,65,11]
+list1.append([74,85])
+print(list1)
+# May create nested list
+
+
+
+
+#extend()
+
+list2=[85,65,11]
+list2.extend([74,85])
+print(list2)
+#Extends the list directly
